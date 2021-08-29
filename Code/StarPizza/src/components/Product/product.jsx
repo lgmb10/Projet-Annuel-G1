8a1eae8ca85d6f537d7f1bbaa9ec854bd090c05d
louis <louis@MacBook-Pro-de-louis.local>
2021-08-29 11:38:25 +0200

    add product

--- Code/StarPizza/src/components/Product/product.jsx
+++ Code/StarPizza/src/components/Product/product.jsx
@@ -1,1 +1,12 @@
 import React from "react";
+
+export const Product = ({ price, image, title, onClick }) => {
+    return (
+        <div onClick={onClick} className="product-container">
+            <img src={image} className="" />
+            <p className="title bold">{title}</p>
+            <p className="price bold">{price}€</p>
+        </div>
+    );
+  };
+  
