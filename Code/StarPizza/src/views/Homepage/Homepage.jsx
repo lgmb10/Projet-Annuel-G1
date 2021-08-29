54965a555a2e946f762ea15cef2052a9e4ba26e6
Flavian Magrit <pro.flavianmagrit@gmail.com>
2021-08-29 10:52:03 +0200

    refactor: delete comments

--- Code/StarPizza/src/views/Homepage/Homepage.jsx
+++ Code/StarPizza/src/views/Homepage/Homepage.jsx
@@ -1,24 +1,21 @@
 import React from "react";
 import { HomepageButton } from "../../components/HomepageButton";
-import { Drawer } from "../../components/Drawer";
+
 import LogoEfficom from "../../assets/logo-EFFICOM.png";
 import LogoStarPizza from "../../assets/logo-star-pizza.png";
 export const Homepage = () => {
   return (
-    // <div className="flex-column jcc aic">
-    //   <img src={LogoStarPizza} />
-    //   <p className="xl-font-size bold italic purple">
-    //     Bienvenue chez Star Pizza
-    //   </p>
-    //   <HomepageButton
-    //     bgColor="#875789"
-    //     image={LogoEfficom}
-    //     title="ÉTUDIANT EFFICOM"
-    //   />
-    //   <HomepageButton bgColor="#55945a" title="CLIENT START PIZZA" />
-    // </div>
-    <>
-      <Drawer />
-    </>
+    <div className="flex-column jcc aic">
+      <img src={LogoStarPizza} />
+      <p className="xl-font-size bold italic purple">
+        Bienvenue chez Star Pizza
+      </p>
+      <HomepageButton
+        bgColor="#875789"
+        image={LogoEfficom}
+        title="ÉTUDIANT EFFICOM"
+      />
+      <HomepageButton bgColor="#55945a" title="CLIENT START PIZZA" />
+    </div>
   );
 };
